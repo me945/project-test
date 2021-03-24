f6beaf474228ef25c92caf5fd883c0c6a684230f
= <=>
2021-03-25 02:02:56 +0400

    fix css

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,18 +5,20 @@ const Header = ({ onAdd, idNum }) => {
     const [showAddForm, setAddShowForm] = useState(false)
 
     return (
-        <div className="container">
+        <div className="container ">
             <div className=" bg-dark text-white mt-3">
-                <h5 className="d-felx justify-content-start pt-3 pl-3">
-                    Live Crypto
-                </h5>
-                <span
-                    style={{ cursor: 'pointer' }}
-                    className="d-felx justify-content-end mb-3"
-                    onClick={() => setAddShowForm(!showAddForm)}
-                >
-                    + Add
-                </span>
+                <div className="d-flex justify-content-between ">
+                    <h5 className="d-flex justify-content-start pt-3 pl-3">
+                        Live Crypto
+                    </h5>
+                    <span
+                        style={{ cursor: 'pointer', paddingTop: '2%' }}
+                        className="d-flex justify-content-end mb-3 pr-3 "
+                        onClick={() => setAddShowForm(!showAddForm)}
+                    >
+                        + Add
+                    </span>
+                </div>
             </div>
 
             {showAddForm === true ? <Form onAdd={onAdd} idNum={idNum} /> : null}
